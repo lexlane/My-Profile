97bfe566d7dfa08f6ccced7da061b2fa0fe15969
lexlane <bottomleyalexis@gmail.com>
2023-01-08 13:29:07 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $(".myIcons").hover(function() {
+        $(this).toggleClass("growIcons");
+    });
+});
+
+
+
+
+
+
+function loadRepo() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        results = JSON.parse(this.responseText);
+        for (let i = 0; i < results.length; i++) {
+          console.log(results[i].name);
+          let ul = document.getElementById("repositories");
+          let li = document.createElement("LI");
+          li.className = "list-group-item";
+          let a = document.createElement("a");
+
+          li.appendChild(document.createTextNode(results[i].name));
+          a.appendChild(li);
+          a.setAttribute("href", results[i].html_url);
+          ul.appendChild(a);
+        }
+      }
+    };
+    xhttp.open("GET", "https://api.github.com/users/lexlane/repos", true);
+    xhttp.send();
+  }
